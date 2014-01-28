c9eb5dfd5c5f38790663edc42ce41071368eb690
BigStarUa <hleborezk@bigmir.net>
2014-01-28 16:47:20 +0200

    fffffff

---
+++ Helix/src/ua/mamamusic/accountancy/model/ExternalReportTableCellRenderer.java
@@ -0,0 +1,25 @@
+package ua.mamamusic.accountancy.model;
+
+import java.awt.Component;
+import java.awt.Label;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+
+public class ExternalReportTableCellRenderer extends DefaultTableCellRenderer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,int row,int col) {
+
+	    if(value instanceof Double){
+	    	value = String.format("%1$,.2f", (Double)value);
+	    }
+	    Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);	
+	    return c;
+	}
+}
